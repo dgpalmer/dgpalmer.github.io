53d2d7ff9bcabd4d3beffbf9eabc7a19d2778db8
Donovan Palmer <donovan@donovanpalmer.net>
2025-05-10 12:14:17 -0700

    downgrading to v3 tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        verde: "#abccba",
+        gris: "#1e211f",
+        oscuror: "#141414",
+        dWhite: "#FFEEFF",
+      },
+    },
+  },
+  plugins: [],
+};
