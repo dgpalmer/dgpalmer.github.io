676a282f1e299144d1e7fc32de81129e37e3fc43
Donovan Palmer <donovan@donovanpalmer.net>
2025-05-10 16:51:25 -0700

    header almost done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,27 @@ export default function Home() {
       <div className="flex flex-col sections">
         <div className="section flex flex-col w-full bg-slate-400 p-4">
           <div>
-            <h1 className="text-2xl">my homepage goes here</h1>
+            <h1 className="text-2xl">my homepage heading</h1>
           </div>
         </div>
         <div className="section flex flex-col w-full bg-slate-200 p-4">
-          <h2 className="text-2xl">my homepage goes here</h2>
+          <h2 className="text-2xl">interests</h2>
           <div className="block w-full h-24 bg-red">something here</div>
         </div>
         <div className="section flex flex-col w-full bg-slate-400 p-4">
-          <h2 className="text-2xl">my homepage goes here</h2>
+          <h2 className="text-2xl">skills</h2>
+          <div className="block w-full h-24 bg-red">something here</div>
+        </div>
+        <div className="section flex flex-col w-full bg-slate-200 p-4">
+          <h2 className="text-2xl">current</h2>
+          <div className="block w-full h-24 bg-red">something here</div>
+        </div>
+        <div className="section flex flex-col w-full bg-slate-400 p-4">
+          <h2 className="text-2xl">past</h2>
+          <div className="block w-full h-24 bg-red">something here</div>
+        </div>
+        <div className="section flex flex-col w-full bg-slate-200 p-4">
+          <h2 className="text-2xl">repos</h2>
           <div className="block w-full h-24 bg-red">something here</div>
         </div>
       </div>
