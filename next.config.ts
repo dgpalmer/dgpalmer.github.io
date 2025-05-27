2b7ceb4773386e56cf566dfdcc8ab26119c7c854
Donovan Palmer <donovan@donovanpalmer.net>
2025-05-26 20:40:56 -0700

    initial deploy workflow

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import createMDX from '@next/mdx';
 import type { NextConfig } from 'next';
 
-// const isProd = process.env.NODE_ENV === 'production'
-const isProd = false;
+const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
   output: 'export',
